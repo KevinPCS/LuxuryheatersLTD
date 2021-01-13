ce6ffd28f08f01f7ebf42a149cec626b67259c80
K. Clever <kevin.clever@hotmail.de>
2021-01-13 20:12:17 +0100

    added registration-form component to routing and added headline for registration component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
   {
     path:"registration",
     component: RegistrationComponent,
+  },
+  {
+    path:"registration-form",
+    component: RegistrationFormComponent,
   },
   {
     path:"",
