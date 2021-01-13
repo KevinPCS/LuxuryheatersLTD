ac5c17f996a4cf2153483de58f662435d6856e70
K. Clever <kevin.clever@hotmail.de>
2021-01-13 19:47:28 +0100

    added registration-form component and added first input field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegistrationComponent } from './registration/registration.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    RegistrationFormComponent
   ],
   imports: [
     BrowserModule,
