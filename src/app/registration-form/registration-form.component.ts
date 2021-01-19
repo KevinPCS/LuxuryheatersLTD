5e46ede193484fece12aba5118098546726ab197
K. Clever <kevin.clever@hotmail.de>
2021-01-19 20:26:02 +0100

    replaced static text with observable (serial number instructions in the right table)

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-registration-form',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationFormComponent implements OnInit {
 
+  instructions: Observable<string> | undefined;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.instructions = of(`Ut lobortis risus eget odio hendrerit, a feugiat nisl bibendum. 
+                    Maecenas pretium orci ut ligula iaculis fermentum. Sed sodales vel risus eu dictum. 
+                    Fusce volutpat, sapien eget pharetra vestibulum.`);
   }
 
 }
