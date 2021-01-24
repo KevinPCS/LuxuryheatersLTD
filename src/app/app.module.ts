cb38d0e8b5fe878e61035d45de06a36cff1f414d
K. Clever <kevin.clever@hotmail.de>
2021-01-24 16:05:56 +0100

    Added license agreement component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,14 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatButtonModule} from '@angular/material/button';
+import { LicenseAgreementComponent } from './license-agreement/license-agreement.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegistrationComponent,
-    RegistrationFormComponent
+    RegistrationFormComponent,
+    LicenseAgreementComponent,
   ],
   imports: [
     BrowserModule,
