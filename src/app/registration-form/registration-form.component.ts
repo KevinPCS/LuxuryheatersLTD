8680f620fb3cc622028f27f1ba4be6a761c4b845
K. Clever <kevin.clever@hotmail.de>
2021-01-27 19:43:59 +0100

    updated validation and tried to validate passwords (still WIP)

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -3,6 +3,7 @@ import {
   AbstractControl,
   FormBuilder,
   FormGroup,
+  //  ValidationErrors,
   ValidatorFn,
   Validators,
 } from '@angular/forms';
@@ -15,6 +16,7 @@ import { Observable, of } from 'rxjs';
 export class RegistrationFormComponent implements OnInit {
   instructions: Observable<string>;
   licenseAgreement: Observable<string>;
+
   registrationForm: FormGroup = this.formBuilder.group({
     name: this.formBuilder.control('', Validators.required),
     email: this.formBuilder.control('', [
@@ -23,7 +25,7 @@ export class RegistrationFormComponent implements OnInit {
     ]),
     city: this.formBuilder.control('', Validators.required),
     street: this.formBuilder.control('', Validators.required),
-    zipCode: this.formBuilder.control('', Validators.required),
+    zipCode: this.formBuilder.control(0, Validators.required),
     password1: this.formBuilder.control('', [
       Validators.required,
       this.checkPasswords,
@@ -32,8 +34,19 @@ export class RegistrationFormComponent implements OnInit {
       Validators.required,
       this.checkPasswords,
     ]),
+
     serial: this.formBuilder.control('', Validators.required),
+
+    checkbox: this.formBuilder.control(false, Validators.requiredTrue),
   });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.instructions = of(`Ut lobortis risus eget odio hendrerit, a feugiat nisl bibendum. 
+                    Maecenas pretium orci ut ligula iaculis fermentum. Sed sodales vel risus eu dictum. 
+                    Fusce volutpat, sapien eget pharetra vestibulum.`);
+  }
 
   checkPasswords(): ValidatorFn {
     return (control: AbstractControl): { [key: string]: any } | null => {
@@ -46,11 +59,25 @@ export class RegistrationFormComponent implements OnInit {
     };
   }
 
-  constructor(private formBuilder: FormBuilder) {}
-
-  ngOnInit(): void {
-    this.instructions = of(`Ut lobortis risus eget odio hendrerit, a feugiat nisl bibendum. 
-                    Maecenas pretium orci ut ligula iaculis fermentum. Sed sodales vel risus eu dictum. 
-                    Fusce volutpat, sapien eget pharetra vestibulum.`);
+  openDialog(): void {
+    console.log(this.registrationForm);
   }
+
+  /*Password Check (WIP)
+
+
+  checkPasswords() {
+    if (this.registrationForm.hasError('passwordMismatch'))
+      this.password2.setErrors([{ passwordMismatch: true }]);
+    else this.password2.setErrors(null);
+  }
+
+  passwordMatchValidator: ValidatorFn = (
+    formGroup: FormGroup
+  ): ValidationErrors | null => {
+    if (formGroup.get('password1').value === formGroup.get('password2').value)
+      return null;
+    else return { passwordMismatch: true };
+  };
+ */
 }
