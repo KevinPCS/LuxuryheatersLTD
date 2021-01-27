c511de01785ea62a3ed7b97a29840a1d93db4e91
K. Clever <kevin.clever@hotmail.de>
2021-01-27 16:16:11 +0100

    updated form and field validation and updated colors accordingly

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { Observable, of } from 'rxjs';
-import { RegistrationDataService } from './registration-data.service';
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
@@ -9,48 +14,43 @@ import { RegistrationDataService } from './registration-data.service';
 })
 export class RegistrationFormComponent implements OnInit {
   instructions: Observable<string>;
-  registrationForm: FormGroup;
   licenseAgreement: Observable<string>;
+  registrationForm: FormGroup = this.formBuilder.group({
+    name: this.formBuilder.control('', Validators.required),
+    email: this.formBuilder.control('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    city: this.formBuilder.control('', Validators.required),
+    street: this.formBuilder.control('', Validators.required),
+    zipCode: this.formBuilder.control('', Validators.required),
+    password1: this.formBuilder.control('', [
+      Validators.required,
+      this.checkPasswords,
+    ]),
+    password2: this.formBuilder.control('', [
+      Validators.required,
+      this.checkPasswords,
+    ]),
+    serial: this.formBuilder.control('', Validators.required),
+  });
 
-  constructor(
-    private registrationDataService: RegistrationDataService,
-    private formBuilder: FormBuilder
-  ) {}
+  checkPasswords(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const passwordMatches =
+        this.registrationForm.get('password1').value ===
+        this.registrationForm.get('password2').value;
+      return passwordMatches
+        ? null
+        : { passwordMatches: { value: control.value } };
+    };
+  }
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.instructions = of(`Ut lobortis risus eget odio hendrerit, a feugiat nisl bibendum. 
                     Maecenas pretium orci ut ligula iaculis fermentum. Sed sodales vel risus eu dictum. 
                     Fusce volutpat, sapien eget pharetra vestibulum.`);
-
-    this.registrationForm = this.formBuilder.group({
-      name: this.formBuilder.control(
-        this.registrationDataService.registrationData.name,
-        Validators.required
-      ),
-      email: this.formBuilder.control(
-        this.registrationDataService.registrationData.email,
-        Validators.required
-      ),
-      city: this.formBuilder.control(
-        this.registrationDataService.registrationData.city,
-        Validators.required
-      ),
-      street: this.formBuilder.control(
-        this.registrationDataService.registrationData.street,
-        Validators.required
-      ),
-      zipCode: this.formBuilder.control(
-        this.registrationDataService.registrationData.zipCode,
-        Validators.required
-      ),
-      password: this.formBuilder.control(
-        this.registrationDataService.registrationData.password,
-        Validators.required
-      ),
-      serial: this.formBuilder.control(
-        this.registrationDataService.registrationData.serial,
-        Validators.required
-      ),
-    });
   }
 }
